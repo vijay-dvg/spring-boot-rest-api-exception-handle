569445691d9c24089a6db537ad994fbd0f1095ec
vijay <vijay.dvg88@gmail.com>
2019-02-15 21:05:41 -0600

    Adding the exception handling files

---
+++ src/main/java/com/vijay/springboot/application/SpringBootRestApiExceptionHandleApplication.java
@@ -0,0 +1,14 @@
+package com.vijay.springboot.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiExceptionHandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiExceptionHandleApplication.class, args);
+	}
+
+}
+
